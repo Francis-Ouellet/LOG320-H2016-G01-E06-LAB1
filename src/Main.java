8f3887f39a63e8b51e1ba5c29f2ee773e5869621
unknown <AM38020@LOGTI-A3326-23.ens.ad.etsmtl.ca>
2016-01-11 16:00:33 -0500

    Programme de base pour lire les listes de mots et utiliser un algorithme de comparaison. Ajout des listes de mots.

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,106 @@
+import java.util.Date;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.text.Normalizer;
 
 public class Main {
 	public static void main(String[] args) {
-        /*	TODO
-         * 	1. Ouvrir les deux fichiers en paramètres et les placer dans deux String Arrays (dictionnaire et mots)
-         * 	2. Traiter les chaines pour enlever les espaces et les accents
-         * 	3. compteur total
-         * 	4. Pour chaque mot
-         * 	5.		compteur courant
-         * 	6. 		Pour chaque mot du dictionnaire de même longueur que le mot
-         * 	7.			Appeler l'algo
-         * 	8. 			Si annagramme
-         * 					incrementer compteur courant
-         * 	9. 		Afficher le nombre d'annagrames du mot
-         * 			incrementer 
-         * */
+        String[] dictionary = null;
+		String[] words = null;
+		String[] sanitizedWords = null;
+		int totalAnagrams = 0;
+		Date start = new Date();
+		Date end = new Date();
 		
-    }
+		// Récupère les fichiers en paramètres
+		if (args.length > 0){
+			dictionary = readFile(args[0]);
+			sanitizedWords = words = readFile(args[1]);
+		}
+		
+		// Enlève les accents et les espaces
+		if(dictionary != null){
+			for (String string : dictionary) {
+				string = sanitize(string);
+			}
+		}
+
+		// Enlève les accents et les espaces		
+		if(sanitizedWords != null){
+			for (String string : dictionary){
+				string = sanitize(string);
+			}
+		}
+		
+		start.getTime();
+		// Pour chaque mot à trouver
+		for (int i = 0; i < sanitizedWords.length; i++){
+			int numberOfMatches = 0;
+			// Pour chaque mot du dictionnaire
+			for(String dictionnaryWord : dictionary){
+				// Si les deux mots sont de même longueur
+				if(sanitizedWords[i].length() == dictionnaryWord.length()){
+					if(AlgoBase()){
+						numberOfMatches++;
+					}
+				}
+			}
+			System.out.println("Il y a " + numberOfMatches + " anagramme(s) du mot " + words[i]);
+			totalAnagrams += numberOfMatches;
+		}
+		end.getTime();
+		
+		System.out.println("Il y a un total de " + totalAnagrams + " anagrammes");
+		System.out.println("Temps d'exécution : " + (end.getTime() - start.getTime()) + " secondes");
+	}
 	
-	private Boolean AlgoBase(){
+	private static String[] readFile(String filePath){
+		String file = "";
+		try{
+			FileInputStream stream = new FileInputStream(filePath);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+			String line = "";
+			
+			while ((line = reader.readLine()) != null){
+				file += line + ";";
+			}
+			
+		}catch(Exception e){}
+		return file.split(";");
+	}
+	
+	private static String sanitize(String s){
+		// Enlever les espaces
+		s = s.replaceAll("\\s", "");
+		/*
+			Code emprunté :
+			Permet d'enlever les accents d'une string
+			Source : http://stackoverflow.com/questions/15190656/easy-way-to-remove-utf-8-accents-from-a-string/15190787#15190787
+		*/
+		s = Normalizer.normalize(s, Normalizer.Form.NFD);
+		s = s.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+		/* Fin du code emprunté */
+		
+		return s;
+	}
+	
+	private static Boolean AlgoBase(){
 		
 		return false;
 	}
 	
-	private Boolean AlgoFrancis(){
+	private static Boolean AlgoFrancis(){
 		
 		return false;
 	}
 	
-	private Boolean AlgoMarco(){
+	private static Boolean AlgoMarco(){
 		
 		return false;
 	}
 	
-	private Boolean AlgoGuy(){
+	private static Boolean AlgoGuy(){
 		
 		return false;
 	}
