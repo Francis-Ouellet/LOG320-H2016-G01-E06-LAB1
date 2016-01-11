1212c089804898e3756b234c6e768435b8d82fc9
unknown <AM38020@LOGTI-A3326-23.ens.ad.etsmtl.ca>
2016-01-11 14:31:48 -0500

    Structure de base

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+
+public class Main {
+	public static void main(String[] args) {
+        /*	TODO
+         * 	1. Ouvrir les deux fichiers en paramètres et les placer dans deux String Arrays (dictionnaire et mots)
+         * 	2. Traiter les chaines pour enlever les espaces et les accents
+         * 	3. compteur total
+         * 	4. Pour chaque mot
+         * 	5.		compteur courant
+         * 	6. 		Pour chaque mot du dictionnaire de même longueur que le mot
+         * 	7.			Appeler l'algo
+         * 	8. 			Si annagramme
+         * 					incrementer compteur courant
+         * 	9. 		Afficher le nombre d'annagrames du mot
+         * 			incrementer 
+         * */
+		
+    }
+	
+	private Boolean AlgoBase(){
+		
+		return false;
+	}
+	
+	private Boolean AlgoFrancis(){
+		
+		return false;
+	}
+	
+	private Boolean AlgoMarco(){
+		
+		return false;
+	}
+	
+	private Boolean AlgoGuy(){
+		
+		return false;
+	}
+}
