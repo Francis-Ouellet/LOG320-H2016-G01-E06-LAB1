2e2be24d2e4c45ea8630944588ac95af2f3da76a
unknown <AM38020@LOGTI-A3326-19.ens.ad.etsmtl.ca>
2016-01-18 16:37:27 -0500

    Combinaison des algos Marco et Francis. Optimisations pour les calculs de hash et sum

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Date;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
@@ -19,7 +18,8 @@ public class Main {
 		// Recupere les fichiers en parametres
 		if (args.length > 0){
 			dictionary = readFile(args[0]);
-			sanitizedWords = words = readFile(args[1]);
+			sanitizedWords = readFile(args[1]);
+			words = readFile(args[1]);
 		}
 		
 		// Enleve les accents et les espaces
@@ -37,12 +37,14 @@ public class Main {
 		}
 		
 		start = System.currentTimeMillis();
+		/*
+		// Traitement i,j pour chaque j 
 		// Pour chaque mot a trouver
 		for (int i = 0; i < sanitizedWords.length; i++){
 			int numberOfMatches = 0;
 			// Pour chaque mot du dictionnaire
 			for(int j = 0; j < dictionary.length; j++){
-				if(AlgoMarco(sanitizedWords[i], dictionary[j])){
+				if(AlgoBase(sanitizedWords[i], dictionary[j])){
 					numberOfMatches++;
 					//System.out.println(dictionary[j]);
 				}
@@ -50,6 +52,42 @@ public class Main {
 			System.out.println("Il y a " + numberOfMatches + " anagramme(s) du mot " + words[i]);
 			totalAnagrams += numberOfMatches;
 		}
+		*/
+		
+		// Traitement i pour chaque i, puis j pour chaque j
+		// Parcours des mots pour lesquels on cherche un anagramme
+		for(int i = 0; i < sanitizedWords.length; i++){
+			int numberOfMatches = 0;
+			char[] array1 = sanitizedWords[i].toCharArray();
+			// Calcul du hash pour le mot source de l'anagramme
+			long hash1 = ToNumericHash(array1);
+			if(hash1 != -1){
+				// Calcul de la somme des lettres pour le mot source de l'anagramme
+				int checkSum1 = getArraySum(array1);
+				// Parcours des mots du dictionnaire
+				for(int j = 0; j < dictionary.length; j++){
+					// Si les deux mots ont la même longueur
+					if(sanitizedWords[i].length() == dictionary[j].length()){
+						char[] array2 = dictionary[j].toCharArray();
+						// Calcul de la somme des lettres pour le mot du dictionnaire
+						int checkSum2 = getArraySum(array2);
+						// Si les deux mots ont la même somme, ils ont le potentiel de contenir les mêmes lettres
+						if(checkSum1 == checkSum2){
+							// Calcul du hash pour le mot du dictionnaire 
+							long hash2 = ToNumericHash(dictionary[j].toCharArray());
+							if(hash2 != -1){
+								// Si les hash sont identiques, il s'agit d'un anagramme
+								if(hash1 == hash2)
+									numberOfMatches++;
+							}
+						}
+					}
+				}
+				System.out.println("Il y a " + numberOfMatches + " anagramme(s) du mot " + words[i]);
+				totalAnagrams += numberOfMatches;
+			}
+		}
+		
 		end = System.currentTimeMillis();
 		
 		System.out.println("Il y a un total de " + totalAnagrams + " anagrammes");
@@ -58,16 +96,25 @@ public class Main {
 	
 	private static String[] readFile(String filePath){
 		String file = "";
+		FileInputStream stream = null;
+		BufferedReader reader = null;
 		try{
-			FileInputStream stream = new FileInputStream(filePath);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-			String line = "";
+			 stream = new FileInputStream(filePath);
+			 reader = new BufferedReader(new InputStreamReader(stream));
+			 String line = ""; 
+			 while ((line = reader.readLine()) != null){
+				 file = file + line + ";";
+			 }
 			
-			while ((line = reader.readLine()) != null){
-				file += line + ";";
-			}
-			
-		}catch(Exception e){}
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		finally{
+			try{
+			reader.close();
+			stream.close();
+			}catch(Exception e){}
+		}
 		return file.split(";");
 	}
 	
@@ -83,10 +130,11 @@ public class Main {
 		s = s.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
 		/* Fin du code emprunte */
 		
-		return s;
+		return s.toLowerCase();
 	}
 	
 	private static Boolean AlgoBase(String string1, String string2){
+		if(string1.length() == string2.length()){
             for(int i = 0; i < string1.length(); i++){
                 boolean foundChar = false;
                 int string2Length = string2.length();
@@ -120,17 +168,27 @@ public class Main {
                 return false;
             
             return true;
+		}
+		else
+			return false;
 	}
 	
 	//  Chaque nombre plus grand que 1 est soit un nombre premier, soit un multiple de nombres premiers
 	//	J'assigne les 26 nombres premiers aux 26 lettres de l'alphabet
-	//  Chaque mot possï¿½de donc une valeur numï¿½rique unique, peu importe l'ordre de ses lettres (abb (2*3*3 = 18) = bab (3*2*2 = 18))
+	//  Chaque mot possede donc une valeur numerique unique, peu importe l'ordre de ses lettres (abb (2*3*3 = 18) = bab (3*2*2 = 18))
 	private static Boolean AlgoFrancis(String word1, String word2){
 		if(word1.length() == word2.length()){
+			
+			char[] chars1 = word1.toCharArray();
+            char[] chars2 = word2.toCharArray();
+            
+            if(getArraySum(chars1) != getArraySum(chars2)){
+                return false;
+            }
 			// on s'assure que toutes les lettres sont majuscules, sinon le hash ne fonctionnerait pas
 			// aussi, les majuscules arrivent en premier dans la table ASCII, on utilise donc des plus petits chiffres
-			long hash1 = ToNumericHash(word1.toUpperCase().toCharArray());
-			long hash2 = ToNumericHash(word2.toUpperCase().toCharArray());
+			long hash1 = ToNumericHash(chars1);
+			long hash2 = ToNumericHash(chars2);
 			if(hash1 == -1 || hash2 == -1)
 				return false;
 			return hash1 == hash2;
@@ -142,9 +200,9 @@ public class Main {
 	private static long ToNumericHash(char[] word){
 		long value = 1L;
 		for(int i = 0; i < word.length; i++ ){
-			// Si le caractï¿½re est dans la portï¿½e des lettres majuscules en ASCII 
-			if (word[i] >= 65 && word[i] <= 90){
-				value = value * PRIMES[word[i] - 65];
+			// Si le caractere est dans la portee des lettres minuscules en ASCII 
+			if (word[i] >= 97 && word[i] <= 122){
+				value = value * PRIMES[word[i] - 97];
 			}
 			else
 				return -1;
@@ -153,13 +211,13 @@ public class Main {
 		
 	}
 	
-        private static int getArraySum(char[] array){
-            int sum = 0;
-            for(int i = 0; i < array.length; i++){
-                    sum += array[i];
-            }
-            return sum;
+    private static int getArraySum(char[] array){
+        int sum = 0;
+        for(int i = 0; i < array.length; i++){
+                sum += array[i];
         }
+        return sum;
+    }
         
 	private static Boolean AlgoMarco(String word1, String word2){
                 int word1Length = word1.length();
