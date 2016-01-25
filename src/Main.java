ea4bf8326504d931143dd71dcf61a301c494ed58
unknown <AM38020@LOGTI-A3324-24.ens.ad.etsmtl.ca>
2016-01-25 15:36:32 -0500

    resultat demo

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ import java.io.InputStreamReader;
 import java.text.Normalizer;
 import java.util.ArrayList;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
 public class Main {
 	
 	private static int[] PRIMES = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151};
@@ -42,8 +40,8 @@ public class Main {
 				sanitizedWords[i] = sanitize(sanitizedWords[i]);
 			}
 		}
-		
 		start = System.currentTimeMillis();
+		
 		/*
 		// Traitement i,j pour chaque j 
 		// Pour chaque mot a trouver
